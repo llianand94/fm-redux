7683fc30d294b468fb2dee134d8139edf9d0e947
Andrey Shapovalov <llianandr94@gmai.com>
2021-12-15 10:50:34 +0200

    Add Todo reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,50 @@
 import ACTION_TYPES from "../actions/actionTypes";
 const initialState= {
   count:0,
-  step:1
+  step:1,
+  tasks:[{
+    id:0,
+    text:'test',
+    isDone:false
+  }]
 };
-
+let serial = 1;
 function reducer (prevState=initialState, action){
   const {count, step}= prevState;
   const {newStep} = action;
   switch (action.type) {
+    case ACTION_TYPES.CREATE_TASK:{ 
+      const {tasks} = prevState; 
+      const {values} = action;    
+      return {
+        ...prevState,
+        tasks:[...tasks,
+          { ...values,id:serial++}]
+      }
+    }
+    case ACTION_TYPES.UPDATE_TASK:{
+      const {id,values} = action;
+      const {tasks} = prevState;
+      const newTasks = tasks.map((task)=>{
+        if(task.id===id){
+          return {...task, ...values}
+        }
+        return {...task}
+      })
+      return{
+        ...prevState,
+        tasks: newTasks,
+      }
+    }
+    case ACTION_TYPES.DELETE_TASK:{
+      const {id} = action;
+      const {tasks} = prevState;
+      const newTasks = tasks.filter(task=>task.id!==id)
+      return {
+        ...prevState,
+        tasks: newTasks
+      }
+    }
     case ACTION_TYPES.INCREMENT:
       return {
         ...prevState,
