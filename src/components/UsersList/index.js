71650d3544de450548e9a9c99c5b3a8e3eda2f45
Andrey Shapovalov <llianandr94@gmai.com>
2022-02-02 09:57:11 +0200

    Use Selector UseDispatch

--- src/components/UsersList/index.js
+++ src/components/UsersList/index.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import {  useDispatch, useSelector } from 'react-redux';
 import * as UserActions from '../../actions/createUsers';
 
 const UsersList = (props) => {
-  const {users, isFetching,error,getUsersReq} = props;
-  useEffect(() => {
-    getUsersReq()
-  }, [getUsersReq]);
+  const {users, isFetching,error} = useSelector(({users})=> users);
+  const dispatch = useDispatch();
+  const getUsersReq = ({limit,offset}={})=>dispatch(UserActions.getUsersRequest({limit,offset}));
+
+  useEffect(() => { getUsersReq() },[]);
   return (
     <section>
       <h2>Users list</h2>
@@ -23,8 +24,4 @@ const UsersList = (props) => {
 }
 
 
-const mapStateToProps = ({users})=> users;
-const mapDispatchToProps = (dispatch) =>({
-  getUsersReq:({limit,offset}={})=>dispatch(UserActions.getUsersRequest({limit,offset}))
-});
-export default connect(mapStateToProps,mapDispatchToProps)(UsersList);
+export default UsersList;
